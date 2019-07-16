2d5b6fbe9b84d5fe336815cdc40fbbd0f69a201a
Natany Caxeta <natanycaxeta@icloud.com>
2019-07-16 16:42:10 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+class EyesOnMe extends React.Component {
+
+    handlerOnFocus = () => console.log('Good!')
+    handleOnBlur = () => console.log('Hey! Eyes on me!')
+
+    render(){
+        return (
+
+            <div>
+                <button onFocus={this.handlerOnFocus} onBlur={this.handleOnBlur}></button>
+            </div>
+
+        )
+    } 
+}
+
+export default EyesOnMe
