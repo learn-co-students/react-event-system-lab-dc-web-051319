8922653aa8b83fa7a4f96d1ebc3543387d14f1d3
Emily Bolten <emilybolten@gmail.com>
2019-07-16 10:58:59 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,35 @@
+import React from 'react'
+
 // Code EyesOnMe Component Here
+
+class EyesOnMe extends React.Component {
+
+    focus = () => {
+        console.log('Good!')
+    }
+
+    blur = () => {
+        console.log('Hey! Eyes on me!')
+    }
+
+    render() {
+        return(
+
+            <div>
+                     
+                <button
+                    onFocus={this.focus}
+                    onBlur={this.blur}
+                >
+                
+                Button
+                
+                </button>
+            
+            </div>
+
+        )
+    }
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,26 @@
+import React from 'react'
+import { throws } from 'assert';
+
 // Code Keypad Component Here
+
+class Keypad extends React.Component {
+   
+    handleEvent = () => {
+        console.log('Entering password...')
+    }
+
+    render(){
+        return(
+            <div>
+            
+                <input
+                type='password'
+                onKeyUp={this.handleEvent}
+                />
+
+            </div>
+        )
+    }
+}
+
+export default Keypad
