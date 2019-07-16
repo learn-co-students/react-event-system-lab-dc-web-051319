a89f581baff12d31711d84a65e6d820bbfc3679b
Your Name <you@example.com>
2019-07-16 15:43:30 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+class EyesOnMe extends React.Component{
+  
+  onFocusEventText = () => {
+    console.log("Good!");
+  }
+
+  onBlurEventText = () => {
+    console.log("Hey! Eyes on me!");
+  }
+
+  render(){
+    return(
+      <button onFocus = {this.onFocusEventText} onBlur = {this.onBlurEventText}/>
+    )
+  }
+}
+export default EyesOnMe
